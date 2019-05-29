bc912da7d5ea44789d8dbf02fda5e8f99e5cbf09
Julian Piron <pironj27@gmail.com>
2019-05-28 20:50:50 -0700

    updated logic and routes.

--- server.js
+++ server.js
@@ -2,38 +2,26 @@
 // =============================================
 var express = require("express");
 
-// Sets up the Express App
-// =============================================================
-var app = express();
 var PORT = process.env.PORT || 8080;
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Set Handlebars.
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var mysql = require("mysql");
+// Import routes and give the server access to them.
+var routes = require("./controllers/catsController.js");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8080,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: "burger_db"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-
-  console.log("connected as id " + connection.threadId);
-});
+app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
