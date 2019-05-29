ea1148fa1c3b5207316a3d1e15b518719c971f0e
Julian Piron <pironj27@gmail.com>
2019-05-28 17:28:06 -0700

    adding files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#Eat-Da-Burger
+# Eat-Da-Burger
 Eat-Da-Burger! is a restaurant app that lets users input the names of burgers they'd like to eat.
 
 Whenever a user submits a burger's name, your app will display the burger on the left side of the page -- waiting to be devoured.
@@ -9,7 +9,7 @@ Your app will store every burger in a database, whether devoured or not.
 
 uses Node and MySQL to query and route data in your app, and Handlebars to generate your HTML.
 
-##Technologies:
+## Technologies:
 * MySQL
 * Node
 * Express
---
+++ server.js
@@ -0,0 +1,42 @@
+// Dependencies
+// =============================================
+var express = require("express");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 8080,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: "burger_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+// Log (server-side) when our server has started
+  console.log("App listening on PORT " + PORT);
+});
