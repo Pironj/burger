c7093fda8c51d45258a8615740efd1cfb1fbdae6
Julian Piron <pironj27@gmail.com>
2019-06-03 17:35:38 -0700

    added css

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
