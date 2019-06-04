4a41d816d1deb06d436a5f13d1a21d8a0ba561e6
Julian Piron <pironj27@gmail.com>
2019-06-03 20:27:39 -0700

    update JAWSdb connection

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 require("dotenv").config();
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: "burger_db"
+  });
+}
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: "burger_db"
-});
 
 // Make connection.
 connection.connect(function(err) {
